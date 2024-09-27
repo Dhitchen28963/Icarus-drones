3e26d9ae4190a2575b1eb4b23661643807db7177
David Hitchen <hitchen28963@yahoo.com>
2024-09-27 12:39:56 +0000

    Added products app, models and fixtures

---
+++ products/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Category, Product
+
+# Admin for Category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name')
+
+# Admin for Product
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku', 'name', 'category', 'price', 'rating', 
+        'image', 'color', 'rotors', 'speed', 'weight',
+        'flight_time', 'camera', 'camera_quality', 'collision_avoidance', 
+        'gps', 'control_range', 'max_altitude', 'wind_resistance', 
+        'material', 'remote_control', 'mobile_app_support', 
+        'warranty', 'compatibility', 'package_contents', 
+        'drones_included', 'drone_model', 'accessories_included'
+    )
+    list_filter = ('category', 'color', 'camera', 'gps')
+    search_fields = ('name', 'sku', 'category__name')
+    list_editable = ('price', 'rating', 'color')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
---
+++ products/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+    rotors = models.IntegerField(null=True, blank=True)
+    speed = models.CharField(max_length=50, null=True, blank=True)
+    weight = models.CharField(max_length=50, null=True, blank=True)
+    flight_time = models.CharField(max_length=50, null=True, blank=True)
+    camera = models.CharField(max_length=10, null=True, blank=True)
+    camera_quality = models.CharField(max_length=100, null=True, blank=True)
+    collision_avoidance = models.CharField(max_length=10, null=True, blank=True)
+    gps = models.CharField(max_length=10, null=True, blank=True)
+    control_range = models.CharField(max_length=50, null=True, blank=True)
+    max_altitude = models.CharField(max_length=50, null=True, blank=True)
+    wind_resistance = models.CharField(max_length=50, null=True, blank=True)
+    material = models.CharField(max_length=100, null=True, blank=True)
+    remote_control = models.CharField(max_length=10, null=True, blank=True)
+    mobile_app_support = models.CharField(max_length=50, null=True, blank=True)
+    warranty = models.CharField(max_length=50, null=True, blank=True)
+    compatibility = models.CharField(max_length=254, null=True, blank=True)
+    package_contents = models.TextField(null=True, blank=True)
+    drones_included = models.IntegerField(null=True, blank=True)
+    drone_model = models.CharField(max_length=254, null=True, blank=True)
+    accessories_included = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
