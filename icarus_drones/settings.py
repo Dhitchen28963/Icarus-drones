c87766b319e85b8c0b666b7ffd634b2f264f7d36
David Hitchen <hitchen28963@yahoo.com>
2024-10-31 20:17:46 +0000

    Add logging configuration and environment variable loading from env.py in settings.

--- icarus_drones/settings.py
+++ icarus_drones/settings.py
@@ -12,22 +12,25 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import logging.config
+
+# Load environment variables from env.py if it exists
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=#zpnlnqb+ye_fo$lz+jre0b!#5xb+5!(ju!59y)g^i^m=%&$q'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')  # Fallback in case env var is missing
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dhitchen289-icarusdrone-5dxslm03s3y.ws.codeinstitute-ide.net']
-
 
 # Application definition
 
@@ -103,10 +106,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    # Needed to log in via the standard Django admin
     'django.contrib.auth.backends.ModelBackend',
-
-    # 'allauth' specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
@@ -124,10 +124,7 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'icarus_drones.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -135,10 +132,7 @@ DATABASES = {
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -154,22 +148,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
