1ed0b90fcc92bf9e5d64d53235054b8a9a259e8b
David Hitchen <hitchen28963@yahoo.com>
2024-11-22 05:01:05 +0000

    Added URL routes for issue reporting, response handling, and staff management

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('report_issue/<order_number>/', views.report_order_issue, name='report_order_issue'),
+    path('manage_issues/', views.manage_issues, name='manage_issues'),
+    path('respond_to_issue/<int:issue_id>/', views.respond_to_issue, name='respond_to_issue'),
+    path('messages/', views.messages_view, name='messages'),
+    path('manage-staff/', views.manage_staff, name='manage_staff'),
 ]
